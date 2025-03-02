d90892347eb9a6dcbad4ff1729bd90d20fb95b96
Mihnea Savin <mihnea.savin@gmail.com>
2025-03-02 21:20:20 +0000

    changed eda to 0.55 probability

--- main.py
+++ main.py
@@ -154,25 +154,24 @@ lemmatizer = WordNetLemmatizer()
 
 eda_augmentor = EDA()
 
-def apply_eda(text):
+def apply_eda(text, prob=0.55):
    # Ensure text is valid
     if not isinstance(text, str) or text.strip() == "":
         return text  # Return unchanged if empty or NaN
 
     num_words = len(text.split())
 
-    # Apply each augmentation with a 40% probability
-    if random.random() < 0.4:
+    # Apply each augmentation with a 70% probability
+    if random.random() < prob:
         text = eda_augmentor.synonym_replacement(text)  # Synonym Replacement
-    if random.random() < 0.4:
+    if random.random() < prob:
         text = eda_augmentor.random_insertion(text)  # Random Insertion
-    if num_words >= 2 and random.random() < 0.4:
+    if num_words >= 2 and random.random() < prob:
         text = eda_augmentor.random_swap(text)  # Random Swap
-    if random.random() < 0.4:
+    if random.random() < prob:
         text = eda_augmentor.random_deletion(text)  # Random Deletion
 
     return text
-
 
 
 
